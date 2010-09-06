39cff2c7af531098ee03577ec387281c6ac461c7
Tom Nielsen <tomn@tomn-desktop.(none)>
2010-09-06 16:36:38 +0100

    trying to make image n-dimensionsal

--- Image.hs
+++ Image.hs
@@ -12,35 +12,35 @@ import Data.Bits
 import Foreign.Storable.Tuple
 
 data Image s a where
-     ImArray :: (Storable a, NDims s) => Int -> Int -> SV.Vector a -> Image s a
+     ImArray :: (Storable a, NDims s ~ n) => Vec n Int -> SV.Vector a -> Image s a
      ImFmap :: (a->b) -> Image s a -> Image s b
 
-at :: Image s a -> (Int,Int) -> a
+at :: Image s a -> Vec n Int -> a
 at (ImFmap f im) xy = f $ im `at` xy
 at (ImArray w h arr) (x,y) = arr `SV.index` (w*y+x)
 
-imageSize :: Image s a -> (Int,Int)
+imageSize :: Image s a -> Vec n Int
 imageSize (ImFmap _ im) = imageSize im
 imageSize (ImArray x y _) = (x,y)             
 
 --unfoldrN :: Storable b => Int -> (a -> Maybe (b, a)) -> a -> (Vector b, Maybe a)	Source
-fillImage :: (Storable a, NDims s) => Int -> Int -> (Int -> Int -> a) -> Image s a
+fillImage :: (Storable a, NDims s ~ n) => Vec n Int -> (Vec n Int -> a) -> Image s a
 fillImage w h f = ImArray w h $ fst $ SV.unfoldrN (w*h) unf (0,0) 
    where unf (x,y) | x < w = Just (f x y, (x+1, y))
                    | y < (h-1) = Just (f 0 (y+1), (1, y+1))
                    | otherwise = Nothing
 
-fillImageLists :: (Storable a, NDims s) => Int -> Int -> [[a]] -> Image s a
+fillImageLists :: (Storable a, NDims s ~ Two) => Int -> Int -> [[a]] -> Image s a
 fillImageLists w h lsts = ImArray w h $ SV.concat $ map SV.pack lsts
 
-loadPNG :: (NDims s, ND s ~ Two) => FilePath -> IO (Image s (Int,Int,Int))
+loadPNG :: (NDims s~ Two) => FilePath -> IO (Image s (Int,Int,Int))
 loadPNG fp = do
   gdim <- GD.loadPngFile fp
   (w,h) <- GD.imageSize gdim
   pts <- forM [0..w-1] $ \x-> forM [0..h-1] $ \y-> toColour `fmap` GD.getPixel (x,y) gdim
   return $ fillImageLists w h $ pts
 
-savePNG :: (NDims s, ND s ~ Two) => FilePath -> Image s (Int,Int,Int) -> IO ()
+savePNG :: (NDims s ~ Two) => FilePath -> Image s (Int,Int,Int) -> IO ()
 savePNG fp img = do
   let (w,h) = imageSize img
   gdIm <- GD.newImage (w,h)
@@ -61,8 +61,7 @@ c 1 = (0,255,0)
 c 2 = (0,0,255)
 c _ = (100,100,100)
 data Flatland
-instance NDims Flatland where 
-    type ND Flatland = Two
+type instance NDims Flatland = Two
 
 type Id a = a -> a
 
